f3e593642e2f536519a2a2a8ce8190a462d15be7
maruf02 <rashiduzzaman99@gmail.com>
2023-05-20 13:33:54 +0600

    new file add

---
+++ VarDetails.js
@@ -0,0 +1,13 @@
+// Here we implement details about variable
+// for integer
+var a=2;
+var b=3;
+var sum=a+b;
+console.log(sum);
+
+// for string for add two string
+
+var a= 'rahim';
+var b= 'Uddin';
+var add=a+ ' ' +b;
+console.log(add);
--- hello.js
+++ hello.js
@@ -1,1 +1,7 @@
+
+
+
+
+
 console.log("abc");
+console.log(2+3);
